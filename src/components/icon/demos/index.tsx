c488d3682303f42a5c0f05b6e1bb1ddff6e85019
YangYongAn <yangyongan@yonyou.com>
2021-11-28 22:22:37 +0800

    update: 更新 icon 的 demo 页面

--- src/components/icon/demos/index.tsx
+++ src/components/icon/demos/index.tsx
@@ -5,6 +5,7 @@ import Space from "../../space";
 
 const template =
 
+
   <>
     <DemoBlock title='基础用法'>
       <Space wrap style={{fontSize: 36}}>
@@ -12,7 +13,21 @@ const template =
         <AppOutline fontSize={72} color={'#3af'}/>
       </Space>
     </DemoBlock>
-
+    <DemoBlock title='大小'>
+      <Space wrap align='center'>
+        <Icon fontSize={12}/>
+        <Icon fontSize={24}/>
+        <Icon fontSize={36}/>
+        <Icon fontSize={48}/>
+      </Space>
+    </DemoBlock>
+    <DemoBlock title='颜色'>
+      <Space wrap style={{fontSize: 36}}>
+        <Icon color='#76c6b8'/>
+        <Icon color='green'/>
+        <Icon color='blue'/>
+      </Space>
+    </DemoBlock>
   </>
 
 avm.render(template, "body");
